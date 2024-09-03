5459c3f0a1d4429afdf84788aea853826e977252
hegdeadithyak <hegdeadithyak@gmail.com>
2024-09-03 23:18:59 +0530

    chore: Update .gitignore and backend/config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 module.exports = {
+    //This will be ignored
     JWT_SECRET: "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxNzkyMTI1MSwiaWF0IjoxNzE3OTIxMjUxfQ.ylhvi22CC6A0EUHrSnLWtcnR1yv1Kye9O4vgI1dn0jc"
 }
